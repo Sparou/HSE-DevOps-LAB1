031443eee2732aef2e292cc13a1d926f9019033b
Nikita Semenov <nsemenov.official@gmail.com>
2023-06-01 21:51:44 +0300

    Updates

--- main.cpp
+++ main.cpp
@@ -1,43 +1,46 @@
 #include <iostream>
 
-int main() {
+int main()
+{
 
-  while (true) {
-      char op;
-      float num1, num2;
+    while (true)
+    {
+        char op;
+        float num1, num2;
 
-      std::cin >> op;
+        std::cin >> op;
 
-      if (op == 'q')
-      {
-        return 0;
-      }
+        if (op == 'q')
+        {
+            return 0;
+        }
 
-      std::cin >> num1 >> num2;
-      
-      switch(op) {
+        std::cin >> num1 >> num2;
+
+        switch (op)
+        {
 
         case '+':
-          std::cout << num1 + num2 << std::endl;
-          break;
+            std::cout << num1 + num2 << std::endl;
+            break;
 
         case '-':
-          std::cout << num1 - num2 << std::endl;
-          break;
+            std::cout << num1 - num2 << std::endl;
+            break;
 
         case '*':
-          std::cout << num1 * num2 << std::endl;
-          break;
+            std::cout << num1 * num2 << std::endl;
+            break;
 
         case '/':
-          std::cout << num1 / num2 << std::endl;
-          break;
+            std::cout << num1 / num2 << std::endl;
+            break;
 
         default:
-          std::cout << "Error! operator is not correct" << std::endl;
-          break;
-      }
+            std::cout << "Error! operator is not correct" << std::endl;
+            break;
+        }
     }
 
-  return 0;
+    return 0;
 }
