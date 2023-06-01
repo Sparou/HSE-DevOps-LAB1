41ecab5f943b412a93864822418f7d01dc058bfb
Nikita Semenov <nsemenov.official@gmail.com>
2023-06-01 20:32:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@ int main() {
       char op;
       float num1, num2;
 
-      std::cout << "Enter operator: +, -, *, /: | or q if you want to exit";
       std::cin >> op;
 
       if (op == 'q')
@@ -14,26 +13,24 @@ int main() {
         return 0;
       }
 
-      std::cout << "Enter two operands: ";
       std::cin >> num1 >> num2;
       
-
       switch(op) {
 
         case '+':
-          std::cout << num1 << " + " << num2 << " = " << num1 + num2 << std::endl;
+          std::cout << num1 + num2 << std::endl;
           break;
 
         case '-':
-          std::cout << num1 << " - " << num2 << " = " << num1 - num2 << std::endl;
+          std::cout << num1 - num2 << std::endl;
           break;
 
         case '*':
-          std::cout << num1 << " * " << num2 << " = " << num1 * num2 << std::endl;
+          std::cout << num1 * num2 << std::endl;
           break;
 
         case '/':
-          std::cout << num1 << " / " << num2 << " = " << num1 / num2 << std::endl;
+          std::cout << num1 / num2 << std::endl;
           break;
 
         default:
